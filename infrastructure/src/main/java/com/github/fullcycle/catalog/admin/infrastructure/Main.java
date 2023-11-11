6b8a0783148c2496100205b8b5974918bf934517
fabipereira <fabio.bpereira@mercadolivre.com>
2023-11-11 20:51:08 -0300

    Start infra

--- infrastructure/src/main/java/com/github/fullcycle/catalog/admin/infrastructure/Main.java
+++ infrastructure/src/main/java/com/github/fullcycle/catalog/admin/infrastructure/Main.java
@@ -1,9 +1,15 @@
 package com.github.fullcycle.catalog.admin.infrastructure;
 
 import com.github.fullcycle.catalog.admin.application.UseCase;
+import com.github.fullcycle.catalog.admin.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ infrastructure/src/main/java/com/github/fullcycle/catalog/admin/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.github.fullcycle.catalog.admin.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.github.fullcycle.catalog.admin")
+public class WebServerConfig {
+}
